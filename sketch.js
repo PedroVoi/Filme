13a357db4c1a53e78c1e5ee757b720b5d500a50a
PedroVoi <160249021+PedroVoi@users.noreply.github.com>
2024-09-23 08:48:00 -0300

    s

---
+++ sketch.js
@@ -0,0 +1,54 @@
+let campoIdade;
+let campoFantasia;
+let campoAventura;
+
+function setup() {
+  createCanvas(800, 400);
+  createElement("h2", "Recomendador de filmes");
+  createSpan("Sua idade:");
+  campoIdade = createInput("5");
+  campoFantasia = createCheckbox("Gosta de fantasia?");
+  campoAventura = createCheckbox("Gosta de aventura?");
+}
+
+function draw() {
+  background("green");
+  let idade = campoIdade.value();
+  let gostaDeFantasia = campoFantasia.checked();
+  let gostaDeAventura = campoAventura.checked();
+  let recomendacao = geraRecomendacao(idade, gostaDeFantasia, gostaDeAventura);
+
+  fill(color(76, 0, 115));
+  textAlign(CENTER, CENTER);
+  textSize(45);
+  text(recomendacao, width / 2, height / 2);
+  
+} 
+
+function geraRecomendacao(idade, gostaDeFantasia, gostaDeAventura) {
+  if (idade >= 10) {
+    if (idade >= 14) {
+      return "Gente Grande";
+    } else {
+      if (idade >= 1) {
+        if(gostaDeFantasia || gostaDeAventura) {
+          return "As aventuras de pi";          
+        } else{
+         return "Shrek 2";
+        }
+      } else {
+        if (gostaDeFantasia) {
+          return "As aventuras de pi";
+        } else {
+          return "Ponyo";
+        }
+      }
+    }
+  } else {
+    if (gostaDeFantasia) {
+      return "O mágico de oz";
+    } else {
+      return "Alice no país das maravilhas";
+    }
+  }
+}
